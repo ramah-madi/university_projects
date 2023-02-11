8dea82ae44763879934a1d276f33d1073f1666a4
ramah-madi <69429801+ramah-madi@users.noreply.github.com>
2023-02-11 22:11:36 +0300

    final project robot lab

---
+++ ROBOTICS LAB/final project robot lab/project 1 Face Characteristics - Get Expression/translations/translation_en_US.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>you happy</source>
+            <comment>Text</comment>
+            <translation type="unfinished">you happy</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>superised</source>
+            <comment>Text</comment>
+            <translation type="unfinished">superised</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>angry</source>
+            <comment>Text</comment>
+            <translation type="unfinished">angry</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>sad</source>
+            <comment>Text</comment>
+            <translation type="unfinished">sad</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>neutral</source>
+            <comment>Text</comment>
+            <translation type="unfinished">neutral</translation>
+        </message>
+    </context>
+</TS>
---
+++ ROBOTICS LAB/final project robot lab/project 2 image recognintion/face recognintion.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="face recognintion" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="epicsax" src="behavior_1/epicsax.ogg" />
+        <File name="heaven1" src="behavior_1/behavior_1/heaven1.ogg" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_ar_SA" src="translations/translation_ar_SA.ts" language="ar_SA" />
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
